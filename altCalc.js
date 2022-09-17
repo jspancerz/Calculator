eddfd79c60d295fb483e7f1d02c4299ee0dc01ce
Jeff Pancerz <pancerzj@gmail.com>
2022-09-16 20:44:43 -0500

    finished most of the calculator logic

--- altCalc.js
+++ altCalc.js
@@ -1,4 +1,3 @@
-//dom objects to manipulate
 const display = document.querySelector( '.calculator-display' );
 const equalsBtn = document.querySelector( '.equals' );
 const allClearBtn = document.querySelector( '.clear' );
@@ -9,61 +8,68 @@ const subtractBtn = document.querySelector( '.subtract' );
 const divideBtn = document.querySelector( '.divide' );
 const multiplyBtn = document.querySelector( '.multiply' );
 
-//storage to perform calculations on.
-let firstOperand = null;
-let mathOperator = null;
-let secondOperand = null;
+let currentValue = '';
+let mathOperator = '';
+let previousValue = '';
 
-//creates clickable buttons for all the number keys, updates the display, assigns values to first/second operand.
 numberBtns.forEach( ( number ) => {
-    number.addEventListener( "click", () => {
-        display.textContent += number.innerText
-        if ( mathOperator === null ) {
-            firstOperand = parseInt( display.innerText )
-        } else if ( mathOperator === '' ) {
-            secondOperand = firstOperand
-
-        }
+    number.addEventListener( 'click', function ( e ) {
+        setNumber( e.target.textContent )
+        display.textContent = currentValue;
     } )
-} );
-
-//creates clickable buttons for all the operator keys, updates display, and assigns value to math operator.
-operatorBtns.forEach( ( operator ) => {
-    operator.addEventListener( "click", () => {
-        display.textContent = operator.innerText
-        mathOperator = operator.innerText
-    } )
-} );
-
-
-//equals calls the appropriate function attached to the string inside mathOperator, then sets firstOperand to the result and updates the display. 
-equalsBtn.addEventListener( 'click', () => {
-    if ( mathOperator === null ) {
-        display.innerText = 'need operator'
-    } else if ( mathOperator === '+' ) {
-        display.innerText = add( firstOperand, secondOperand )
-        firstOperand = parseInt( display.innerText )
-    } else if ( mathOperator === '-' ) {
-        display.innerText = subtract( firstOperand, secondOperand )
-        firstOperand = parseInt( display.innerText )
-    } else if ( mathOperator === '*' ) {
-        display.innerText = multiply( firstOperand, secondOperand )
-        firstOperand = parseInt( display.innerText )
-    } else if ( mathOperator === '/' ) {
-        display.innerText = divide( firstOperand, secondOperand )
-        firstOperand = parseInt( display.innerText )
-    }
-} );
-
-//clears all memory of previous inputs
-allClearBtn.addEventListener( 'click', () => {
-    display.innerText = null;
-    firstOperand = null;
-    mathOperator = null;
-    secondOperand = null;
 } )
 
-//math functions
+function setNumber( num ) {
+    if ( currentValue.length <= 5 ) {
+        currentValue += num
+        console.log( `this is the currentValue: ${ currentValue }` )
+    }
+};
+
+operatorBtns.forEach( ( operator ) => {
+    operator.addEventListener( 'click', function ( e ) {
+        setOperator( e.target.textContent )
+        display.textContent = operator.textContent
+    } )
+} )
+
+function setOperator( op ) {
+    mathOperator = op
+    previousValue = currentValue;
+    console.log( `this is the previousValue: ${ previousValue }` )
+    currentValue = '';
+    console.log( `this is the currentValue: ${ currentValue }` )
+};
+
+equalsBtn.addEventListener( 'click', function () {
+    if ( mathOperator !== '' ) {
+        calculate()
+        display.textContent = currentValue
+    }
+} )
+
+function calculate() {
+    previousValue = Number( previousValue );
+    currentValue = Number( currentValue );
+
+    if ( mathOperator === '+' ) {
+        currentValue = add( previousValue, currentValue )
+    } else if ( mathOperator === '-' ) {
+        currentValue = subtract( previousValue, currentValue )
+    } else if ( mathOperator === '*' ) {
+        currentValue = multiply( previousValue, currentValue )
+    } else if ( mathOperator === '/' ) {
+        currentValue = divide( previousValue, currentValue )
+    }
+};
+
+allClearBtn.addEventListener( 'click', () => {
+    display.textContent = '';
+    currentValue = '';
+    mathOperator = '';
+    previousValue = '';
+} )
+
 function add( x, y ) {
     return result = x + y
 };
@@ -76,13 +82,3 @@ function multiply( x, y ) {
 function divide( x, y ) {
     return result = x / y
 };
-
-
-
-
-
-
-
-
-
-
